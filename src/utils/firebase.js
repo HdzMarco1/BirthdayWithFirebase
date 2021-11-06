7afa326a60bdc26beacd717817cb29479df99902
HdzMarco1 <eh76396@gmail.com>
2021-11-06 15:13:41 -0600

    guia 11 finalizada

---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYEDBsw5UWKW2W-5WaBWzDE1oM-x2YkJo",
+  authDomain: "birthday-41ea4.firebaseapp.com",
+  projectId: "birthday-41ea4",
+  storageBucket: "birthday-41ea4.appspot.com",
+  messagingSenderId: "331009592339",
+  appId: "1:331009592339:web:f3c68f2404d5f0c77d83e6",
+};
+
+firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+export default firebase;
